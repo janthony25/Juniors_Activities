29f36d5ae3cfb46a819d1f3784d6abcc9a8896d2
Russel Acosta <robertannerusselacosta.learning@gmail.com>
2025-11-13 12:52:24 +0800

    added a js file on act 2 vanilla activities

---
+++ russ_act/vanilla_activities/act2/index.js
@@ -0,0 +1,10 @@
+var greetings = "hello";
+var newArray = [];
+        //i = 5            //0 < 5
+for(var i = greetings.length-1; i > ; i--)
+{
+	// console.log(greetings);
+	newArray[] = greetings[i];
+}
+
+console.log(newArray);

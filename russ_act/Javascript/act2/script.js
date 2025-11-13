12f8bc632a2c615bff024d0c2773263f873eae2e
Russel Acosta <robertannerusselacosta.learning@gmail.com>
2025-11-13 13:46:42 +0800

    Added a function when button is clicked, it changes the text in the paragraph

---
+++ russ_act/Javascript/act2/script.js
@@ -0,0 +1,6 @@
+function changeText() {
+	console.log("Change text!");
+	var paragraphTextChange = document.getElementById("change-text");
+
+	paragraphTextChange.innerHTML = "Text has been changed!";
+}

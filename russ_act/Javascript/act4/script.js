3baf22d2ff6bfbea9a30edec8f9437bf4ea9090a
Russel Acosta <robertannerusselacosta.learning@gmail.com>
2025-11-14 16:40:43 +0800

    created a function for toggling the paragraph text using if else

--- russ_act/Javascript/act4/script.js
+++ russ_act/Javascript/act4/script.js
@@ -1,8 +1,37 @@
 console.log("----This is Activity 4----");
 function clickToToggle() {
 
-	
+	//Method #1
+	// document.getElementById("toggle-switch").addEventListener('click', function() {
+	// 	var paragraph = document.getElementById('main-paragraph');
+	// 	if (paragraph.style.display === 'block') 
+	// 	{
+	// 		paragraph.style.display = 'none'; //changes the display style of the paragraph			
+	// 		console.log("The display change into: none");
+	// 	} else {
+	// 		paragraph.style.display = "block"; 
+	// 		console.log("The display change into: block");
+	// 	}
+	// });
+
+	//Method #2 --> This is one is working and much simplier than the first method.
+	var paragraph = document.getElementById("main-paragraph");
+	if (paragraph.style.display == "block")//check if the display style is == None
+	{
+		paragraph.style.display = "none"; //changes the dispaly style of the paragraph
+		console.log("The display style changed into: none");
+	} else {
+		paragraph.style.display = "block"; 
+		console.log("The display style changed into: block");
+	}
+
 }
+// note:
+/*
+
+- How to change the external css, rather than inline style....
+*/
 
 
-console.log("----This is Activity 4----");
+
+

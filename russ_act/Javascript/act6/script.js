68fe6c94a6b023d92c0376b0929412e21f588596
Russel Acosta <robertannerusselacosta.learning@gmail.com>
2025-11-15 11:56:19 +0800

    add comments to identify the procedures of each code

--- russ_act/Javascript/act6/script.js
+++ russ_act/Javascript/act6/script.js
@@ -1,14 +1,14 @@
 console.log("Hiyuuu, world!");
 function showHidePassword() {
-
+	// initial view of password will be dots/asterisks
 	var password = document.getElementById("password");
 
 	if (password.type == "password") {
-		password.type = "text";
+		password.type = "text"; //will show the asterisks and return it as text
 		console.log("Password type change into: " + password.type);
 	} 
 	else {
-		password.type = "password";
+		password.type = "password"; //will return back to password type
 		console.log("Password type change into: " + password.type);
 	}
 }

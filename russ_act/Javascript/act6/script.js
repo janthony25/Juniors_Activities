d9062ade0cd833fa0d4d79812e0ac1cf61256ee5
Russel Acosta <robertannerusselacosta.learning@gmail.com>
2025-11-15 11:54:52 +0800

    added the functionality of initial password, when first clicked, it will show as text and when clicked again, it will turn to password again

--- russ_act/Javascript/act6/script.js
+++ russ_act/Javascript/act6/script.js
@@ -1,4 +1,14 @@
 console.log("Hiyuuu, world!");
 function showHidePassword() {
 
+	var password = document.getElementById("password");
+
+	if (password.type == "password") {
+		password.type = "text";
+		console.log("Password type change into: " + password.type);
+	} 
+	else {
+		password.type = "password";
+		console.log("Password type change into: " + password.type);
+	}
 }
